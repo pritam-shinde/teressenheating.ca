9a9edf610b5113cf3ff9d07f01391de8d052ebd9
Pritam Shinde <pritamshinde.success@gmail.com>
2022-11-05 18:04:10 +0530

    last update 051120221803 (upgrade from Next Js  12 To Next Js 13)

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-
-module.exports = {
-    siteUrl: 'http://teressenheating.ca/',
-    generateRobotsTxt: true,
-    generateIndexSitemap: false,
-    sitemapSize: 9999,
-  }
