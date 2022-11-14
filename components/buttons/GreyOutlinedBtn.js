ab6f672f9534ddbff6b8a11873074ec6ce8ef68a
Pritam Shinde <pritamshinde.success@gmail.com>
2022-11-14 10:04:31 +0530

    empty

--- components/buttons/GreyOutlinedBtn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GreyOutlinedBtn = () => {
-  return (
-    <div>GreyOutlinedBtn</div>
-  )
-}
-
-export default GreyOutlinedBtn
--- pages/your-local-lennox-dealer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LocalLennoxDealer = () => {
-  return (
-    <>LocalLennoxDealer</>
-  )
-}
-
-export default LocalLennoxDealer
