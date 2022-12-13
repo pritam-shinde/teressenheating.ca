34d9bfe5bcdcec5db94880104f14ab0b213669b1
Pritam Shinde <44087724+pritam-shinde@users.noreply.github.com>
2022-12-13 18:20:38 +0530

    update gtag ---Adi

--- pages/_app.js
+++ pages/_app.js
@@ -30,8 +30,25 @@ function MyApp({ Component, pageProps }) {
   return (<>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="google-site-verification" content="EyzpH-lXbXN8fg7XPxUm_lfNy29_AIRYbcgFqJbJ8Kw" />
     </Head>
     <Header />
+          <Script id="google-tag-manager" strategy="afterInteractive">
+      {
+        `(function (w, d, s, l, i) {
+          w[l] = w[l] || []; w[l].push({
+            'gtm.start':
+              new Date().getTime(), event: 'gtm.js'
+          }); var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+              'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-TFB5PTT');`
+      }
+    </Script>
+
+<noscript dangerouslySetInnerHTML={{
+      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TFB5PTT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
     <Component {...pageProps} />
     {
       scrollHeight > 100 ? <Box className='d-flex justify-content-center align-items-center rounded-circle' style={{ height: "3rem", width: "3rem", backgroundColor: "var(--red)", position: "fixed", right: "1%", bottom: "2%" }}>
