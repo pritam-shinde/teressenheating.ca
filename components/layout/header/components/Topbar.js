604e35b02750a5872fd2dddf44743e445efdc6e0
anki96 <44087724+pritam-shinde@users.noreply.github.com>
2023-02-23 16:59:20 +0530

    changes

--- components/layout/header/components/Topbar.js
+++ components/layout/header/components/Topbar.js
@@ -24,63 +24,13 @@ const Topbar = () => {
                                 <Grid item xs={12} sm={9} lg={8}>
                                     <Box>
                                         <Grid container spacing={5}>
-                                            <Grid item xs={12} sm={8} className="d-flex justify-content-end align-items-center">
+                                            <Grid item xs={12}  className="d-flex justify-content-end align-items-center">
                                                 <strong className='callText'><small className='text--grey'>24/7 Emergency Service</small></strong>
                                                 <Box ml={2} mt={1}>
                                                     <GreyFilledBtn anchor={true} btnlink="tel:604-363-6622" btnTitle="604-363-6622" />
                                                 </Box>
                                             </Grid>
-                                            <Grid item xs={12} sm={4} className="d-flex justify-content-end align-items-center">
-                                                <Box>
-                                                    <List className='d-flex p-0 m-0'>
-                                                        <ListItem className='p-0 me-1'>
-                                                            <a href="https://www.facebook.com/profile.php?id=100088431184715">
-                                                                <ListItemAvatar>
-                                                                    <Avatar>
-                                                                        <FaFacebookF />
-                                                                    </Avatar>
-                                                                </ListItemAvatar>
-                                                            </a>
-                                                        </ListItem>
-                                                        <ListItem className='p-0 me-1'>
-                                                            <a href="https://twitter.com/BaymanShawqi">
-                                                                <ListItemAvatar>
-                                                                    <Avatar>
-                                                                        <FaTwitter />
-                                                                    </Avatar>
-                                                                </ListItemAvatar>
-                                                            </a>
-                                                        </ListItem>
-                                                        <ListItem className='p-0 me-1'>
-                                                            <a href="https://www.pinterest.com.au/teressenheating/">
-                                                                <ListItemAvatar>
-                                                                    <Avatar>
-                                                                        <FaPinterestP />
-                                                                    </Avatar>
-                                                                </ListItemAvatar>
-                                                            </a>
-                                                        </ListItem>
-                                                        <ListItem className='p-0 me-1'>
-                                                            <a href="https://g.page/r/CdlSgou1yAB-EB0/review">
-                                                                <ListItemAvatar>
-                                                                    <Avatar>
-                                                                        <Google/>
-                                                                    </Avatar>
-                                                                </ListItemAvatar>
-                                                            </a>
-                                                        </ListItem>
-                                                       <ListItem className='p-0 me-1'>
-                                                        <a href="https://www.instagram.com/terrssenheating/">
-                                                            <ListItemAvatar>
-                                                                <Avatar>
-                                                                    <FaInstagram/>
-                                                                </Avatar>
-                                                            </ListItemAvatar>
-                                                        </a>
-                                                    </ListItem>
-                                                    </List>
-                                                </Box>
-                                            </Grid>
+                                           
                                         </Grid>
                                     </Box>
                                 </Grid>
